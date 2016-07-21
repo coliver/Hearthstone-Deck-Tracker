29709fe5e721212ea629b5ccbc79cdfcfd86a5e1
Alexander Zeier <epikz37@gmail.com>
2016-07-21 12:09:35 +0200

    log version, fix account url, bump version

--- Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
+++ Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
@@ -56,8 +56,8 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.15.12.0")]
-[assembly: AssemblyFileVersion("0.15.12.0")]
+[assembly: AssemblyVersion("0.15.12.20")]
+[assembly: AssemblyFileVersion("0.15.12.20")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
